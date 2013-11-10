e82e8fd070a97f667c51d631cdf97ab3822cc98a
Lucas Crisman <kordero@gmail.com>
2013-11-10 05:33:56 -0300

    dispatcher improvements

--- src/enbici/MVC/Dispatcher.php
+++ src/enbici/MVC/Dispatcher.php
@@ -3,40 +3,69 @@
 namespace enbici\MVC;
 
 class Dispatcher extends Object {
+    protected $model;
+    protected $view;
+    protected $controller;
+
+    public function __construct($options = array()) {
+        $options = array_merge(array(
+            'base'       => $this->guessBasePath(),
+            'model'      => '/model',
+            'view'       => '/view',
+            'controller' => '/controller'
+        ), $options);
+
+        extract($options);
+        $this->model = is_readable($base.$model) ? $base.$model : $model;
+        $this->view = is_readable($base.$view) ? $base.$view : $view;
+        $this->controller = is_readable($base.$controller) ? $base.$controller : $controller;
+    }
+
+    protected function guessBasePath() {
+        $path = dirname(__FILE__);
+        $regex = '/(.*)(?:\\' . DIRECTORY_SEPARATOR . 'vendor' . '\\' . DIRECTORY_SEPARATOR . ').*/';
+        if (preg_match($regex, $path, $matches)) {
+            return $matches[1];
+        } else {
+            return dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))));
+        }
+    }
+
+
     public function autoload($name) {
-       if (class_exists($name)) {
-           return null;
-       }
-       if (preg_match('/Model$/', $name)) {
-           if ($name == 'Model') {
-              $file = MVC_MODEL . DIRECTORY_SEPARATOR . 'Model.php';
-           } else {
-               $name = preg_replace('/Model$/', '', $name);
-               $file = MVC_MODEL . DIRECTORY_SEPARATOR . $name . '.php';
+        if (class_exists($name)) {
+            return null;
+        }
+        if (preg_match('/Model$/', $name)) {
+            if ($name == 'Model') {
+                $file = $this->model . DIRECTORY_SEPARATOR . 'Model.php';
+            } else {
+                $name = preg_replace('/Model$/', '', $name);
+                $file = $this->model . DIRECTORY_SEPARATOR . $name . '.php';
+            }
+        }
+        if (preg_match('/Controller$/', $name)) {
+            if ($name == 'Controller') {
+                $file = $this->controller . DIRECTORY_SEPARATOR . 'Controller.php';
+            } else {
+                $name = preg_replace('/Controller$/', '', $name);
+                $file = $this->controller . DIRECTORY_SEPARATOR . $name . '.php';
            }
-       }
-       if (preg_match('/Controller$/', $name)) {
-           if ($name == 'Controller') {
-              $file = MVC_CONTROLLER . DIRECTORY_SEPARATOR . 'Controller.php';
-           } else {
-               $name = preg_replace('/Controller$/', '', $name);
-               $file = MVC_CONTROLLER . DIRECTORY_SEPARATOR . $name . '.php';
-           }
-       }
-       if (preg_match('/View$/', $name)) {
-           if ($name == 'View') {
-              $file = MVC_VIEW . DIRECTORY_SEPARATOR . 'View.php';
-           } else {
-               $name = preg_replace('/View$/', '', $name);
-               $file = MVC_VIEW . DIRECTORY_SEPARATOR . $name . '.php';
-           }
-       }
+        }
+        if (preg_match('/View$/', $name)) {
+            if ($name == 'View') {
+                $file = $this->view . DIRECTORY_SEPARATOR . 'View.php';
+            } else {
+                $name = preg_replace('/View$/', '', $name);
+                $file = $this->view . DIRECTORY_SEPARATOR . $name . '.php';
+            }
+        }
        
-       if (file_exists($file)) {
-           require_once($file);
-           return true;
-       }
-       return false;
+        if (file_exists($file)) {
+            require_once($file);
+            return true;
+        }
+        return false;
     }
 
     public function run($params) {
@@ -55,6 +84,6 @@ class Dispatcher extends Object {
             }
             return true;
         }
-	return false;
+        return false;
     }
 }
